b50a4960a1ced5587fbafedc32be84a6c5a379d0
kwonth211 <kwonth211@naver.com>
2020-11-25 18:44:23 +0900

    complete backbone

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,7 +1,7 @@
 export = {
   type: "mysql",
   host: process.env.HOST,
-  port: Number(process.env.PORT),
+  port: process.env.PORT,
   username: process.env.USERNAME,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
